20ae4ed6045b92e5c12099fcf6cf7bbfa0c21c94
Programador <programador@haskell.com.br>
2020-09-29 08:15:56 -0300

    conexões corrigidas em database.php

--- config/database.php
+++ config/database.php
@@ -39,34 +39,6 @@ return [
 
     'connections' => [
 
-        'sqlite' => [
-            'driver' => 'sqlite',
-            'url' => env('DATABASE_URL'),
-            'database' => env('DB_DATABASE', database_path('database.sqlite')),
-            'prefix' => '',
-            'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
-        ],
-
-        'mysql_produto' => [
-            'driver' => 'mysql',
-            'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST_PRODUTO', '127.0.0.1'),
-            'port' => env('DB_PORT_PRODUTO', '3306'),
-            'database' => env('DB_DATABASE_PRODUTO', 'produto'),
-            'username' => env('DB_USERNAME_PRODUTO', 'root'),
-            'password' => env('DB_PASSWORD_PRODUTO', ''),
-            'unix_socket' => env('DB_SOCKET', ''),
-            'charset' => 'utf8mb4',
-            'collation' => 'utf8mb4_unicode_ci',
-            'prefix' => '',
-            'prefix_indexes' => true,
-            'strict' => true,
-            'engine' => null,
-            'options' => extension_loaded('pdo_mysql') ? array_filter([
-                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
-            ]) : [],
-        ],
-        
         'mysql_cliente' => [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
@@ -106,7 +78,35 @@ return [
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
             ]) : [],
         ],
-        
+
+        'mysql_produto' => [
+            'driver' => 'mysql',
+            'url' => env('DATABASE_URL'),
+            'host' => env('DB_HOST_PRODUTO', '127.0.0.1'),
+            'port' => env('DB_PORT_PRODUTO', '3306'),
+            'database' => env('DB_DATABASE_PRODUTO', 'produto'),
+            'username' => env('DB_USERNAME_PRODUTO', 'root'),
+            'password' => env('DB_PASSWORD_PRODUTO', ''),
+            'unix_socket' => env('DB_SOCKET', ''),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'strict' => true,
+            'engine' => null,
+            'options' => extension_loaded('pdo_mysql') ? array_filter([
+                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+            ]) : [],
+        ],
+
+        'sqlite' => [
+            'driver' => 'sqlite',
+            'url' => env('DATABASE_URL'),
+            'database' => env('DB_DATABASE', database_path('database.sqlite')),
+            'prefix' => '',
+            'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
+        ],
+
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
