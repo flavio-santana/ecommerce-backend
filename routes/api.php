34ad09effe017a07cb318b56e9218b1e1fc2ffc1
Programador <programador@haskell.com.br>
2020-09-30 13:09:33 -0300

    inserindo comentários em Route group em api.php

--- routes/api.php
+++ routes/api.php
@@ -72,16 +72,17 @@ Route::get('/pedidos/{pedido}/itens', 'PedidoController@itemPedidos')->name('ite
 // Aqui, temos encapsulado os métodos index(get), store(post), show(get), update(put) e destroy(delete)
 Route::apiResource('/cupons','CupomController');
 
+/*
 Route::group(array('prefix' => 'api/v1', 'before' => 'auth.basic'), function()
 {
-    //Route::resource('/cupons', 'CupomController', array('only' => array('index', 'store', 'show', 'update', 'destroy')));
-    //Route::resource('users', 'UsersController');
+    Route::resource('/cupons', 'CupomController', array('only' => array('index', 'store', 'show', 'update', 'destroy')));
+    Route::resource('users', 'UsersController');
     
     // Aqui, temos encapsulado os métodos index(get), store(post), show(get), update(put) e destroy(delete)
-    //Route::apiResource('/cupons','CupomController');
+    Route::apiResource('/cupons','CupomController');
 
 });
-
+*/
 
 // Aqui, temos encapsulado os métodos index(get), store(post), show(get), update(put) e destroy(delete)
 Route::apiResource('/promocoes','PromocaoController');
